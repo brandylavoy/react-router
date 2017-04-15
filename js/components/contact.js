55dde47b282c89c9952dafc2739b789c1ba71abb
Brandy La Voy <brandylavoy@gmail.com>
2017-04-15 14:33:30 -0700

    router working

---
+++ js/components/contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Link} from 'react-router';
+
+export default function Contact(props) {
+    return (
+        <div>
+        <strong>
+        <Link to={'/contacts/' + props.id}>
+        {props.name}
+        </Link>
+        </strong>
+        &nbsp;
+        {props.phoneNumber}
+        </div>
+    );
+};
